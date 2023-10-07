6a59a21ab8e734a2dcaca4646f709e6a0499c96b
Shreyas Subhash <shreyas.subhash@aymcommerce.com>
2023-10-08 04:51:00 +0530

    update-edit-profile

--- library-management-system/src/app/modules/client/components/profile/profile.component.ts
+++ library-management-system/src/app/modules/client/components/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
+  updateForm: FormGroup;
 
+  constructor(private userService: UserService, private router: Router, private fb: FormBuilder) {
+    this.updateForm = this.fb.group({
+      username: ['', [Validators.required,Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  updateUser() {
+
+  }
 }
